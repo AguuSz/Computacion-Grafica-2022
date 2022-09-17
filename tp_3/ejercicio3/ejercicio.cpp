87f8f9e59f35046b7eae4f2eaf9515d74327c62b
AguuSz <agus.sepu2000@hotmail.com>
2022-09-17 09:56:54 -0300

    Ejercicio 3 terminado

--- tp_3/ejercicio3/ejercicio.cpp
+++ tp_3/ejercicio3/ejercicio.cpp
@@ -1,12 +1,9 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <cmath>
-#include <iostream>
 
 #define MAX_WIDTH 640
 #define MAX_HEIGHT 480
-
-using namespace std;
 
 struct Point
 {
@@ -14,18 +11,18 @@ struct Point
     int y;
 };
 
-int mouseOffsetX = 0, mouseOffsetY = 0;
 Point drawingPoints[] = {
-    0, 0,
-    70, 0,
-    70, 70,
-    80, 70,
-    80, 0,
-    140, 0,
-    140, 50,
-    150, 50,
-    150, 0,
-    200, 0};
+    5, 5,
+    70, 5,
+    70, 75,
+    80, 75,
+    80, 5,
+    140, 5,
+    140, 55,
+    150, 55,
+    150, 5,
+    200, 5,
+    210, 0};
 
 //<<<<<<<<<<<<< Inicialización >>>>>>>>>>>>>
 void iniciar(void)
@@ -44,16 +41,19 @@ void handleKeyboardAction(unsigned char keyPressed, int x, int y)
     {
     case 97:
         // Tecla presionada: a
+        for (int i = 0; i < 11; i++)
+        {
+            drawingPoints[i].x *= 1.2;
+            drawingPoints[i].y *= 1.2;
+        }
         break;
     case 115:
         // Tecla presionada: s
-        break;
-
-    case 100:
-        // Tecla presionada: d
-        break;
-    case 113:
-        // Tecla presionada: q
+        for (int i = 0; i < 11; i++)
+        {
+            drawingPoints[i].x *= 0.8;
+            drawingPoints[i].y *= 0.8;
+        }
         break;
     }
     glutPostRedisplay();
@@ -73,24 +73,40 @@ void draw(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    double theta = M_PI / 4;
-    double dx = 1 / tan(theta);
+    double dx = 1 / tan(M_PI / 4); // Angulo para la cizalla: 45 grados
+    double theta = M_PI / 3;       // Angulo para la rotacion: 30 grados
+    Point rotatedPoints[11];
+    Point cizallaPoints[11];
+    Point temporalPoint;
 
-    glBegin(GL_LINE_STRIP);
-    for (Point point : drawingPoints)
+    for (int i = 1; i < 7; i++)
     {
-        glVertex2d(point.x + 50 + dx * point.y, point.y + 200);
-    }
-    glVertex2d(drawingPoints[9].x + 55, drawingPoints[9].y + 195);
-    glEnd();
 
-    glBegin(GL_LINE_STRIP);
-    for (Point point : drawingPoints)
-    {
-        glVertex2d(point.x + 50 + dx * point.y, -point.y + 190);
+        // Aplicamos cizalla para luego rotar cada uno de los puntos
+        for (int j = 0; j < 11; j++)
+        {
+            temporalPoint = drawingPoints[j];
+            temporalPoint.x += dx * temporalPoint.y;
+            cizallaPoints[j] = temporalPoint;
+            rotatedPoints[j] = rotate(cizallaPoints[j], theta * i);
+        }
+
+        // Dibujamos los puntos una vez rotados
+        glBegin(GL_LINE_STRIP);
+        for (Point point : rotatedPoints)
+        {
+            glVertex2d(point.x + MAX_WIDTH / 2, point.y + MAX_HEIGHT / 2);
+        }
+        glEnd();
+
+        // Dibujado de la rama espejada -> Y negativo
+        glBegin(GL_LINE_STRIP);
+        for (Point point : rotatedPoints)
+        {
+            glVertex2d(point.x + MAX_WIDTH / 2, -point.y + MAX_HEIGHT / 2);
+        }
+        glEnd();
     }
-    glVertex2d(drawingPoints[9].x + 55, drawingPoints[9].y + 195);
-    glEnd();
 
     glFlush();
 }
@@ -102,7 +118,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(MAX_WIDTH, MAX_HEIGHT);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("TP_3 | Ejercicio 1");
+    glutCreateWindow("TP_3 | Ejercicio 3");
     glutDisplayFunc(draw);
     glutKeyboardFunc(handleKeyboardAction);
     iniciar();
