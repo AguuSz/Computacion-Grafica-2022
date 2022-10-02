8486f6a2bfe10f7ec61f91e266dcc6ecf7eebfd0
AguuSz <agus.sepu2000@hotmail.com>
2022-10-02 19:18:17 -0300

    tp_4 added with ejercicio1

---
+++ tp_4/ejercicio1/ejercicio.cpp
@@ -0,0 +1,129 @@
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+#define MAX_WIDTH 640
+#define MAX_HEIGHT 480
+
+using namespace std;
+
+struct point3D
+{
+    float x;
+    float y;
+    float z;
+};
+
+int theta;
+GLenum drawingStyle = GL_LINE;
+
+//<<<<<<<<<<<<< Inicialización >>>>>>>>>>>>>
+void iniciar(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    //    glMatrixMode(GL_PROJECTION);
+    //    gluOrtho2D(-1, 2, 0, 5);
+}
+
+void handleKeyboardAction(unsigned char keyPressed, int x, int y)
+{
+    switch (keyPressed)
+    {
+    case 97:
+        // Tecla presionada: a
+        if (drawingStyle == GL_LINE)
+            drawingStyle = GL_FILL;
+        else
+            drawingStyle = GL_LINE;
+        break;
+    }
+    glutPostRedisplay();
+}
+
+void rotate()
+{
+    theta += 1;
+    if (theta > 360)
+        theta = 0;
+    glutPostRedisplay();
+}
+
+void drawHouse()
+{
+    ifstream file("../files/basicbar.3vn");
+
+    int numVertices, numNormales, numSuperficies;
+    file >> numVertices >> numNormales >> numSuperficies;
+
+    point3D vertices[numVertices], normales[numNormales];
+
+    // Relleno de matrices
+    for (int i = 0; i < numVertices; i++)
+    {
+        file >> vertices[i].x;
+        file >> vertices[i].y;
+        file >> vertices[i].z;
+    }
+    for (int i = 0; i < numNormales; i++)
+    {
+        file >> normales[i].x;
+        file >> normales[i].y;
+        file >> normales[i].z;
+    }
+
+    // Creacion de superficies en base a los vertices ya creados
+    for (int i = 0; i < numSuperficies; i++)
+    {
+        int numVerticesSuperficie;
+        file >> numVerticesSuperficie;
+
+        // Obtengo los vertices de la superficie
+        glBegin(GL_POLYGON);
+        for (int j = 0; j < numVerticesSuperficie; j++)
+        {
+            int puntoADibujar;
+            file >> puntoADibujar;
+            glVertex3d(vertices[puntoADibujar].x / 2, vertices[puntoADibujar].y / 2, vertices[puntoADibujar].z / 2);
+        }
+        glEnd();
+        // Obtengo los vertices de la normal de una superficie
+        // En este ejercicio se ignoran las normales
+        for (int j = 0; j < numVerticesSuperficie; j++)
+        {
+            int normalIndex;
+            file >> normalIndex;
+        }
+    }
+}
+
+//<<<<<<<<<<<<<<<<< Dibujado >>>>>>>>>>>>>>>>
+void draw(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glRotatef(theta, 0, 1, 0);
+    glPolygonMode(GL_FRONT_AND_BACK, drawingStyle);
+
+    drawHouse();
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+//<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(MAX_WIDTH, MAX_HEIGHT);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("TP_4 | Ejercicio 1");
+    glutIdleFunc(rotate);
+    glutDisplayFunc(draw);
+    glutKeyboardFunc(handleKeyboardAction);
+    iniciar();
+    glutMainLoop();
+}
